5eac26c88dd46733b578d7536239af1e56d861ec
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2020-04-06 14:54:26 +0100

    Add files via upload

--- APIs.py
+++ APIs.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 class product:
     def __init__(self,productJson,vendor):
         self.vendor = vendor
@@ -10,3 +13,35 @@ class product:
             self.price = productJson['salePrice']
             self.desc = productJson['name']
     
+class bbSearch:
+    def __init__(self,query):
+        bestBuy = requests.get('https://api.bestbuy.com/v1/products(search='+query+')?format=json&show=image,name,salePrice&apiKey=lfXY4GpC14duGk4N3uGvGD3d')
+        self.result = json.loads(bestBuy.text)
+        self.result = self.result['products']
+        self.products = productList(self.result, 'BestBuy')
+    def display(self):
+        displayProducts(self.products)
+
+class tescoSearch:
+    def __init__(self,query):
+        tesco = requests.get(f"https://dev.tescolabs.com/grocery/products/?query="+query+"&offset=0&limit=13&",
+            headers={
+            "Ocp-Apim-Subscription-Key": 'b1ef4204177a4deb86619436f9e1e7a6'
+            }
+        )
+        self.result = json.loads(tesco.text)
+        self.result = self.result['uk']['ghs']['products']['results']  
+        self.products = productList(self.result, 'Tesco')
+    def display(self):
+        displayProducts(self.products)
+            
+def productList(productList, vendor):
+    listOfProducts = []
+    for i in range(len(productList)):
+        listOfProducts.append(product(productList[i],vendor))
+    return listOfProducts
+
+def displayProducts(productList):
+    for i in range(len(productList)):
+        temp = productList[i]
+        print(temp.desc+"\n Price: £"+str(temp.price)+"\n")
--- main.py
+++ main.py
@@ -1,45 +1,10 @@
-import requests
-import json
 import tkinter 
-from product import product
+from APIs import *
 
 def main():
-    bbProducts = getBestBuy()
-    displayProducts(bbProducts)
-    tescoProducts = getTesco()
-    displayProducts(tescoProducts)
-    
-def getBestBuy():
-    search = input("Enter search query: ")
-    bestBuy = requests.get('https://api.bestbuy.com/v1/products(search='+search+')?format=json&show=image,name,salePrice&apiKey=lfXY4GpC14duGk4N3uGvGD3d')
-    print(bestBuy.status_code) #200 = OK 
-    result = json.loads(bestBuy.text)
-    result = result['products']
-    
-    return productList(result, 'BestBuy')
-
-def getTesco():
-    search = input("Enter search query: ")
-    tesco = requests.get(f"https://dev.tescolabs.com/grocery/products/?query="+search+"&offset=0&limit=13&",
-        headers={
-        "Ocp-Apim-Subscription-Key": 'b1ef4204177a4deb86619436f9e1e7a6'
-        }
-    )
-    print(tesco.status_code)
-    result = json.loads(tesco.text)
-    result = result['uk']['ghs']['products']['results']   
-        
-    return productList(result,'Tesco')
-
-def productList(productList, vendor):
-    listOfProducts = []
-    for i in range(len(productList)):
-        listOfProducts.append(product(productList[i],vendor))
-    return listOfProducts
-
-def displayProducts(productList):
-    for i in range(len(productList)):
-        temp = productList[i]
-        print(temp.desc+"\n Price: £"+str(temp.price)+"\n")
+    bb = bbSearch(input("Enter search query(BestBuy): "))
+    bb.display()
+    tesco = tescoSearch(input("Enter search query(Tesco): "))
+    tesco.display()
         
 main()
